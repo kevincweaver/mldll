2847b052f418d023467e7057aa7f11b088887e4e
kevincweaver <50827775+kevincweaver@users.noreply.github.com>
2020-10-28 05:56:21 -0500

    added comments and sample output

--- script.py
+++ script.py
@@ -1,57 +1,79 @@
-from ctypes import c_void_p, c_double, c_int, cdll
-from numpy.ctypeslib import ndpointer
-import time
+#! python
+# functions are wrapped in mldll.py and implemented in cpp
+
 import numpy as np
+import mldll
 
-mydll = cdll.LoadLibrary('MLDLL.dll')
+matrix1 = np.full((1000,1000), 2, dtype=np.float64)
+matrix2 = np.full((1000,1000), 2, dtype=np.float64)
 
-matrix = np.full((1000,1000), 1, dtype=np.float64)             
-n = matrix.shape[0]
-m = matrix.shape[1]
+# transposes a matrix 
+mldll.transp(matrix1)
 
-matrix2 = np.full((1000,1000), 1, dtype=np.float64)
-n2 = matrix2.shape[0]
-m2 = matrix2.shape[1]
+# transposes a blocked matrix, ie one submatrix at a time such that
+# the processor cache can be better utilized
+mldll.transp2(matrix1)
 
-time1 = time.time()
-transp = mydll.c_transp
-transp.restype = ndpointer(dtype=c_double, shape=(m,n))
-result = transp(c_void_p(matrix.ctypes.data), c_int(n), c_int(m))
-print(result)
-time2 = time.time()-time1
-print("running time in seconds:", time2)
+# multiplies two matricies
+mldll.mmult(matrix1, matrix2)
 
-time1 = time.time()
-transp2 = mydll.c_transp2
-transp2.restype = ndpointer(dtype=c_double, shape=(m,n))
-result = transp(c_void_p(matrix.ctypes.data), c_int(n), c_int(m))
-print(result)
-time2 = time.time()-time1
-print("running time in seconds:", time2)
+# multiplies two matricies, but utilizes transp2() to transform
+# the second matrix beforehand to maximise sequential (column) memory accesses
+mldll.mmult2(matrix1, matrix2)
 
-time1 = time.time()
-mmult = mydll.c_mmult
-mmult.restype = ndpointer(dtype=c_double, shape=(n,m2))
-result = mmult(c_void_p(matrix.ctypes.data), c_int(n), c_int(m),
-               c_void_p(matrix2.ctypes.data), c_int(n2), c_int(m2))
-print(result)
-time2 = time.time()-time1
-print("running time in seconds:", time2)
+# multiplies two matricies in a blocked manner
+mldll.mmult3(matrix1, matrix2)
 
-time1 = time.time()
-mmult2 = mydll.c_mmult2
-mmult2.restype = ndpointer(dtype=c_double, shape=(n,m2))
-result = mmult2(c_void_p(matrix.ctypes.data), c_int(n), c_int(m),
-                c_void_p(matrix2.ctypes.data), c_int(n2), c_int(m2))
-print(result)
-time2 = time.time()-time1
-print("running time in seconds:", time2)
+# multiplies two matricies utilizing transp2() and a blocking strategy
+mldll.mmult4(matrix1, matrix2)
 
-time1 = time.time()
-mmult3 = mydll.c_mmult3
-mmult3.restype = ndpointer(dtype=c_double, shape=(n,m2))
-result = mmult3(c_void_p(matrix.ctypes.data), c_int(n), c_int(m),
-                c_void_p(matrix2.ctypes.data), c_int(n2), c_int(m2))
-print(result)
-time2 = time.time()-time1
-print("running time in seconds:", time2)
+''' SAMPLE OUTPUT:
+[[2. 2. 2. ... 2. 2. 2.]
+ [2. 2. 2. ... 2. 2. 2.]
+ [2. 2. 2. ... 2. 2. 2.]
+ ...
+ [2. 2. 2. ... 2. 2. 2.]
+ [2. 2. 2. ... 2. 2. 2.]
+ [2. 2. 2. ... 2. 2. 2.]]
+transp() executed in:  0.031241178512573242 seconds
+[[2. 2. 2. ... 2. 2. 2.]
+ [2. 2. 2. ... 2. 2. 2.]
+ [2. 2. 2. ... 2. 2. 2.]
+ ...
+ [2. 2. 2. ... 2. 2. 2.]
+ [2. 2. 2. ... 2. 2. 2.]
+ [2. 2. 2. ... 2. 2. 2.]]
+transp2() executed in:  0.03124403953552246 seconds
+[[4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ ...
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]]
+mmult() executed in:  2.203826665878296 seconds
+[[4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ ...
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]]
+mmult2() executed in:  1.3902983665466309 seconds
+[[4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ ...
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]]
+mmult3() executed in:  1.4941833019256592 seconds
+[[4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ ...
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]
+ [4000. 4000. 4000. ... 4000. 4000. 4000.]]
+mmult4() executed in:  1.257746696472168 seconds
+'''
